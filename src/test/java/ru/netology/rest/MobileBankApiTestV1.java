db966ce6efe909d51e063a331ba84045e1ea5ba8
Petr <petr_mameev@mail.ru>
2022-08-08 23:19:02 +0700

    shema error six commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-
+import static org.hamcrest.Matchers.equalTo;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
@@ -21,5 +21,26 @@ class MobileBankApiTestV1 {
               .statusCode(200)
               // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
               .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+    @Test
+    void shouldReturnDemoAccountsJSONSchema() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsValueOfCurrencyIsRUB() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("RUB"))
+        ;
     }
 }
